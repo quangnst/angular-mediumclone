e0c3e6849ebdafc6c1d13cdd0a6b60b77372d960
NghiemQuang <quangnst@gmail.com>
2022-12-12 23:33:55 +0700

    08: create auth module

---
+++ mediumclone/src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { RegisterComponent } from './components/register/register.component';
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forChild(routes)],
+  declarations: [
+    RegisterComponent
+  ]
+})
+export class AuthModule {}
