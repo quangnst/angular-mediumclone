65810516301afeaa90ae11118d429b814ad028dd
NghiemQuang <quangnst@gmail.com>
2022-12-15 21:29:43 +0700

    install store devtools

--- mediumclone/src/app/app.module.ts
+++ mediumclone/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
-
+import { isDevMode } from '@angular/core';
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -13,6 +14,11 @@ import { AuthModule } from './auth/auth.module';
     AppRoutingModule,
     AuthModule,
     StoreModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
